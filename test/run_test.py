262312085f68adcf18e86fd66180d4d05b744c4a
cowboy_niu <q384413898@163.com>
2026-07-09 11:25:35 +0800

    chore(test): 移除测试脚本中的绘图相关代码
    删除了冗余的绘图导入和绘图保存代码，精简测试脚本内容

--- test/run_test.py
+++ test/run_test.py
@@ -1,6 +1,5 @@
 import rsnumpy as np
 import time
-import rsplotlib.pyplot as plt
 
 
 start_time = time.time()
@@ -1671,21 +1670,6 @@ print(b)
 
 print(np.__version__)
 print('\n')
-
-def f(t):
-    return np.exp(-t) * np.cos(2*np.pi*t)
-
-
-t1 = np.arange(0.0, 5.0, 0.1)
-t2 = np.arange(0.0, 5.0, 0.02)
-
-plt.figure()
-plt.subplot(211)
-plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
-
-plt.subplot(212)
-plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
-plt.savefig('./test/out.png')
 
 end_time = time.time()
 
